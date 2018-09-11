a5c3332c08c403b0c3192433bde7cdf92334650f
mila <vorga88@gmail.com>
2018-09-11 15:30:35 +0300

    fix footer visibility on mobile

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,16 @@
 import "../scss/main.scss";
 
-window.addEventListener('load', function() {
-    var controls = {
+window.addEventListener('load', function () {
+    let controls = {
         elements: document.querySelectorAll(".price__controls .controls__link"),
         table: document.querySelector(".table"),
-        addListeners: function() {
-            for(let i=0; i < this.elements.length; i++) {
-                let el= this.elements[i];
+        addListeners: function () {
+            for (let i = 0; i < this.elements.length; i++) {
+                let el = this.elements[i];
                 el.addEventListener('click', e => {
-                    let left = !i ? 6.25 : "-" + (87.5*1 - 6.25);
+                    let left = !i ? 6.25 : "-" + (87.5 * 1 - 6.25);
                     this.table.style.left = left + "vw";
-                    for(var n of this.elements) {
+                    for (var n of this.elements) {
                         n.classList.remove("controls__link--active");
                     }
                     el.classList.add("controls__link--active")
@@ -25,7 +25,7 @@ window.addEventListener('load', function() {
         close: document.querySelector(".menu__close-icon"),
         hero: document.querySelector(".hero"),
 
-        addListeners: function() {
+        addListeners: function () {
             this.open.addEventListener('click', e => {
                 this.element.classList.add("menu--open");
                 this.hero.classList.add("hero--open");
@@ -35,9 +35,50 @@ window.addEventListener('load', function() {
                 this.hero.classList.remove("hero--open");
             });
         }
-    }
+    };
+
+    //add functionality to sliders
+
+    (function () {
+        let fillSlider = document.querySelector(".js-slider-fill");
+        let contrast = document.querySelector(".js-slider-contrast");
+        let image = document.querySelector(".js-upload-img");
+
+        if (fillSlider && contrast && image) {
+            fillSlider.addEventListener('input', (e) => {
+                image.style.filter = `hue-rotate(${fillSlider.value}deg)`;
+            });
+
+            contrast.addEventListener('input', (e) => {
+                image.style.filter = `contrast(${contrast.value}%)`;
+            });
+        }
+
+    })();
+
+    let icons = {
+        els: document.querySelectorAll(".js-upload-icon"),
+        sliders: document.querySelectorAll(".js-slider"),
+
+        addListeners() {
+            this.els.forEach((el, index) => {
+                el.addEventListener('click', () => {
+                    this.els.forEach((icon) => icon.classList.remove("active-icon"));
+                    el.classList.add("active-icon");
+
+                    this.sliders.forEach(slider => slider.style.display = "none");
+                    this.sliders[index].style.display = "block";
+                });
+            })
+        }
+
+    };
+
+
 
     controls.addListeners();
 
     menu.addListeners();
+
+    icons.addListeners();
 });
